5b917c3a01fb685a209fbd47b8d26409e56c18b4
Eric Ruppert <ericeruppert@gmail.com>
2017-07-02 13:22:22 -0400

    init commit

---
+++ src/main/java/org/ist411/group5/RESTApplication.java
@@ -0,0 +1,16 @@
+package org.ist411.group5;
+
+/**
+ *
+ * @author Eric Ruppert
+ */
+public class RESTApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
