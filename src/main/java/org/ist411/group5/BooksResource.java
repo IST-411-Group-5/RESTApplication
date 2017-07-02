dc4d89284e9787b7a8e83f8d3e5735e27f25e479
Susan Tabassum <sttabassum93@gmail.com>
2017-07-03 02:36:42 +0600

    POST method start

--- src/main/java/org/ist411/group5/BooksResource.java
+++ src/main/java/org/ist411/group5/BooksResource.java
@@ -7,8 +7,23 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.core.MediaType;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonWriter;
+import javax.json.JsonReader;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.io.*;
+import java.math.BigDecimal;
 /**
  * REST Web Service
  *
@@ -43,5 +58,39 @@ public class BooksResource {
     @PUT
     @Consumes(MediaType.TEXT_HTML)
     public void putHtml(String content) {
+    }
+    
+    /*
+     *POST method writes an instance of BooksResource to the server
+     */
+    @POST
+    public void postBook() {
+        ArrayList<Book> bookList = new ArrayList();
+        Book book1 = new Book("Brave New World", "Aldous Huxley", 9780060850524L);
+        Book book2 = new Book("Nineteen Eighty-Four", "George Orwell", 9780141182957L);
+        Book book3 = new Book("Lord of the Flies", "William Golding", 9780807218181L);
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+        
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        for (Book book : bookList){
+            arrayBuilder.add(
+                    Json.createObjectBuilder()
+                    .add("Title", book.getTitle())
+                    .add("Author", book.getAuthor())
+                    .add("ISBN", book.getISBN())
+            );
+        }
+        JsonArray bookArray = arrayBuilder.build();
+        try {
+            OutputStream output = new FileOutputStream("Books.json");
+            JsonWriter writer = Json.createWriter(output);
+            writer.writeArray(bookArray);
+            output.close();
+            writer.close();
+        } catch(IOException e){
+            System.out.println("File not found!");
+        }
     }
 }
